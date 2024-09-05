7f80807cf7f0c36184add95296979a8d25ee3070
Gokula krishnan R <towner.mvpproject@gmail.com>
2024-09-05 22:08:24 +0530

    chore: Update tender model to use string type for date fields

--- dist/routor/route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-router.get("/", (req, res) => {
-    res.send("Hello World");
-});
-router.use("/auth", require("./userRoute"));
-router.use("/tender", require("./tenderRoute"));
-module.exports = router;
-//# sourceMappingURL=route.js.map
--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,7 @@ app.use(
       "http://localhost:3001",
       "https://tender-online-web.vercel.app",
       "https://tender-online-admin-ibob.vercel.app",
+      "https://tender-online-web-l5ws.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
